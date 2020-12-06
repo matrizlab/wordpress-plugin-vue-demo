9027347e44fba3d663ae74240c002e0562c9ddb4
matrizlab <info@matrizlab.com>
2020-12-06 13:35:23 +0100

    start plugin

---
+++ wordpress-plugin-vue-demo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wordpress Plugin Vue Demo
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wordpress-plugin-vue-demo
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wordpress_Plugin_Vue_Demo
+ */
+
+// Your code starts here.
